7d91f030ebea5e4472b7414fa2006aa6d9e03c2e
thomas <trd4@me.com>
2022-10-26 14:54:04 -0700

    updating run file

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from email.policy import default
 import os
 import sys
 import argparse
+
 import typing
 from typing import Union
 import yaml
@@ -21,7 +22,8 @@ if __name__ == "__main__":
     parser.add_argument('--settings', type=str, default='settings.yml')
     parser.add_argument('--projection_epsg', type=str, default='EPSG:3857') # pseudo-mercator, which is in meters but not perfect for distance calculations
     parser.add_argument('--default_epsg', type=str, default='EPSG:4326')
-    parser.add_argument('--export_vector', type=bool, default=False)
+    parser.add_argument('--export', action='store_true')
+    parser.add_argument('--sample', action='store_true')
     parser.add_argument(
         "--log-level",
         default=logging.INFO,
@@ -54,6 +56,11 @@ if __name__ == "__main__":
 
     # now into the management code - first want to get the building footprints and the regional statistics
     footprints = get_footprints(city_path=city_path)
+    if config['sample'] == True:
+        logger.info("Segmenting sample for code testing...")
+        footprints = footprints.head(5)
+
+    logger.info("Processing {} footprints...".format(len(footprints)))
     results = manage(footprints, **config)
     logger.info('Saving files....')
     save_data(results, city_path=city_path, **config)
