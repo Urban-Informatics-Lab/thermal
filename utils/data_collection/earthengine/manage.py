a48996b69fd0a5e24d389a1eed347e0f47f541a1
thomas <trd4@me.com>
2022-11-17 11:13:52 -0800

    updating process flow

--- utils/data_collection/earthengine/manage.py
+++ utils/data_collection/earthengine/manage.py
@@ -1,3 +1,6 @@
+from utils.data_collection.earthengine import cmip, dynamicworld, ecostress, elevation, era5, era5_land, landsat8, lst_terra, lst_aqua, sar, sentinel_1C, sentinel_2A, noaa, viirs
+from utils.data_collection.earthengine.dateutil import datelist
+from utils.data_collection.footprint_area import to_featurecollection
 from concurrent.futures import process
 import ee
 from datetime import date
@@ -8,44 +11,41 @@ from typing import Union
 import logging
 logger = logging.getLogger(__name__)
 
-from utils.data_collection.footprint_area import to_featurecollection
-from utils.data_collection.earthengine.dateutil import datelist
 
 # the kinds of data we're going to work with
-from utils.data_collection.earthengine import cmip, dynamicworld, ecostress, elevation, era5, era5_land, landsat8, lst_terra, lst_aqua, sar, sentinel_1C, sentinel_2A, noaa, viirs
+
 
 def manage(
-    footprints: GeoDataFrame,
-    **config):
+        footprints: GeoDataFrame,
+        **config):
     '''Runs all of the specific satellite commands'''
-    ee.Initialize() # may want to hook this up later to config file
+    ee.Initialize()  # may want to hook this up later to config file
     feature_collection = to_featurecollection(footprints, **config)
 
     # each of these data sources should have a common 'run' function with a common interface
     data_classes = [
-        # cmip,
+        cmip,
         # dynamicworld,
         # era5_land,
         # lst_terra,
         # lst_aqua,
-        # ecostress,
         # elevation,
-        era5,
-        noaa,
-        sar,
-        sentinel_2A,
-        sentinel_1C,
-        viirs,
-        landsat8
+        # era5,
+        # noaa,
+        # sar,
+        # sentinel_2A,
+        # sentinel_1C,
+        # viirs,
+        # landsat8
     ]
-    
+
     processed_classes = {}
     for data_class in data_classes:
         classname = data_class.__name__.split('.')[-1]
 
         logger.info("Processing Dataclass: {}".format(classname))
         processed_classes[classname] = data_class.run(
-            feature_collection, 
+            feature_collection,
             **config
         )
 
--- utils/file_management.py
+++ utils/file_management.py
@@ -25,12 +25,12 @@ logger = logging.getLogger(__name__)
 
 
 def initialize(
-    base_dir:str, 
-    data_dir:str, 
-    city:str = None,
-    settings:str = None,
-    commandline_settings:dict = {},
-    ) -> Tuple[dict, str]:
+    base_dir: str,
+    data_dir: str,
+    city: str = None,
+    settings: str = None,
+    commandline_settings: dict = {},
+) -> Tuple[dict, str]:
     """Load global settings and the city specific settings"""
     city_path = generate_structure(data_dir=data_dir, city=city)
 
@@ -39,34 +39,38 @@ def initialize(
 
     if os.path.exists(default_settings_file):
         with open(default_settings_file, 'r') as file:
-            default_settings = { **default_settings, **yaml.safe_load(file) }
-            logger.debug("Default Settings: {}".format(default_settings))
+            default_settings = {**default_settings, **yaml.safe_load(file)}
+            logger.info("Default Settings: {}".format(default_settings))
     else:
         logger.info("Default settings not loaded...")
 
-    settings_file = os.path.join(city_path, settings)
+    settings_file = settings
+    logger.info("Custom Settings Path: {}".format(settings_file))
     custom_settings = {}
 
     if os.path.exists(settings_file):
         with open(settings_file, 'r') as file:
-            custom_settings = custom_settings | yaml.safe_load(file)
-            logger.debug("Custom Settings: {}".format(custom_settings))
+            custom_settings = {**custom_settings, **yaml.safe_load(file)}
+            logger.info("Custom Settings: {}".format(custom_settings))
     else:
         logger.info("Custom settings not loaded...")
 
-    config = { **default_settings, **custom_settings, **commandline_settings }
+    config = {**default_settings, **custom_settings, **commandline_settings}
     return config, city_path
 
-def generate_structure(data_dir:str, city:str = None) -> str:
+
+def generate_structure(data_dir: str, city: str = None) -> str:
     """Returns the city_path which will be used in later computations"""
-    if city is not None: 
+    if city is not None:
         # what we want most of the time
         city_path = os.path.join(data_dir, city)
     else:
         if os.path.exists(data_dir):
-            contents_dir = [d for d in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir,d))]
-            contents_intdir = list(filter(lambda x: x.isnumeric(), contents_dir))
-            contents_int = [ int(x) for x in contents_intdir ]
+            contents_dir = [d for d in os.listdir(
+                data_dir) if os.path.isdir(os.path.join(data_dir, d))]
+            contents_intdir = list(
+                filter(lambda x: x.isnumeric(), contents_dir))
+            contents_int = [int(x) for x in contents_intdir]
 
             if len(contents_int) > 0:
                 city_path = os.path.join(data_dir, str(max(contents_int)+1))
@@ -74,9 +78,10 @@ def generate_structure(data_dir:str, city:str = None) -> str:
                 city_path = os.path.join(data_dir, '1')
         else:
             city_path = os.path.join(data_dir, '1')
-    
+
     os.makedirs(city_path, exist_ok=True)
     return city_path
+
 
 def get_footprints(city_path: str, epsg: str = None) -> str:
     """Check if the building footprints have been provided"""
@@ -93,10 +98,11 @@ def get_footprints(city_path: str, epsg: str = None) -> str:
 
     if epsg is not None:
         footprints = footprints.to_crs(epsg=epsg)
-    
+
     return footprints
 
-def save_data(processed_classes: dict, city_path: str, export:bool = False, custom_selectors:list = [], **kwargs):
+
+def save_data(processed_classes: dict, city_path: str, export: bool = False, custom_selectors: list = [], **kwargs):
     logger.info('Saving...')
     collected_datadir = os.path.join(city_path, 'satellite')
     os.makedirs(collected_datadir, exist_ok=True)
@@ -107,8 +113,8 @@ def save_data(processed_classes: dict, city_path: str, export:bool = False, cust
             # we want this if we're running a larger computation which is going to kill resources
             logger.info(f"Exporting {key} Data to Drive...")
             task = ee.batch.Export.table.toDrive(
-                description=key, 
-                folder=collected_datadir, 
+                description=key,
+                folder=collected_datadir,
                 fileFormat="CSV",
                 selectors=selectors,
                 collection=collection
@@ -120,4 +126,5 @@ def save_data(processed_classes: dict, city_path: str, export:bool = False, cust
             geodf = pd.DataFrame(geemap.ee_to_geopandas(collection))
             retaining_df = geodf.loc[:, selectors]
             retaining_df.to_csv(data_path)
-            logger.info("Sample of {} data:\n{}".format(key, retaining_df.head(5)))
+            logger.info("Sample of {} data:\n{}".format(
+                key, retaining_df.head(5)))
